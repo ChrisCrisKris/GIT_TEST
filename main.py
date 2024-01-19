f3c7f5210205142a0600d3e8efa2d4e3e89dcc0d
Chris Kris <chriskris.internet@gmail.com>
2024-01-19 22:47:52 +0100

    Mon premier commit. Point de départ du projet

---
+++ main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This is a sample Python script.
+
+# Press Maj+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def display(text):
+    print(text)
+    return 1
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print(0.1+0.1+0.1)
+    text1="salut c\'est bien,\tessai\nautre ligne"
+    text2= "c:\\usr\\app\\test.py"
+    display(text1)
+    retour = display(text2)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
